071034b677413c9f1281e034d4c58250a2e7d104
Rafael Baccin <akabacc@gmail.com>
2026-08-12 21:50:35 -0300

    g

--- src/game.lua
+++ src/game.lua
@@ -67,6 +67,13 @@ function love.keypressed(key)
 	if key == "escape" then
 		love.event.quit()
 	end
+	if key == "f" then
+		if love.window.getFullscreen() == false then
+			love.window.setFullscreen(true)
+		else
+			love.window.setFullscreen(false)
+		end
+	end
 end
 
 return game
